784e338a0e044daedc8f3e69427d45a84606587d
Anthony Dwyer <anthonydwyer@gmail.com>
2012-07-04 08:33:57 -0400

    Code commit to work on.

--- app/public/js/gitaapp.js
+++ app/public/js/gitaapp.js
@@ -10,7 +10,10 @@ GitaApp = {
 	},
 
 	upload : function(file, progressCallback) {
-		if (!this.key)
+		var self = this,
+            formData = new FormData();
+
+        if (!this.key)
 			throw {
 				code : 'INVALID_TOKEN',
 				errorString : 'Secret key was not set.  Please try again later.'
@@ -20,12 +23,42 @@ GitaApp = {
 
 		if (progressCallback !== undefined) {
 			xhr.upload.addEventListener('progress', progressCallback, false);
+            xhr.upload.addEventListener('onload', function(){
+                self.displayError({messsage: 'LOADED'});
+            })
 		}
-		xhr.open('PUT', url);
-		xhr.send(file);
-	},
+        formData.append('files', file);
+		xhr.open('POST', url, true);
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xhr.setRequestHeader("X-File-Name", file.fileName);
+        xhr.setRequestHeader("X-File-Size", file.fileSize);
+		xhr.send(formData);
+    },
 
 	download : function(num) {
-		alert('should download a file.')
-	}
+		window.open('data:');
+	},
+
+    displayError: function(error) {
+        var errors = undefined;
+
+        if (!$.isArray(error)) {
+            errors = new Array();
+            errors.push(error);
+        } else {
+            errors = error;
+        }
+
+        for (var i = 0; i < errors.length; ++i) {
+            $('#error > ul').prepend('<li>' + errors[i].message + '</li>');
+        }
+
+        $('#error').show(300, 'linear');
+    },
+
+    clearError: function() {
+        $('#error > ul').html('');
+        $('#error').hide();
+    }
 }
